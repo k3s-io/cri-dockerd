ee2ebf3b953412faac3f06dca89a7d7b62ba1f5b
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2023-08-15 23:35:57 +0300

    go mod vendor

--- vendor/k8s.io/component-base/version/base.go
+++ vendor/k8s.io/component-base/version/base.go
@@ -2,9 +2,9 @@ package version
 
 var (
 	gitMajor = "1"
-	gitMinor = "27"
-	gitVersion   = "v1.27.0-k3s1"
-	gitCommit    = "70ed969180a45a8c4c59e935114171d025eab30d"
+	gitMinor = "28"
+	gitVersion   = "v1.28.0-k3s1"
+	gitCommit    = "5c04389e49f338a7d3aa4f5d2747b998f2d1febd"
 	gitTreeState = "clean"
-	buildDate = "2023-04-11T20:02:01Z"
+	buildDate = "2023-08-15T19:30:37Z"
 )
